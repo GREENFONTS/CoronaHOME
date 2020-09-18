492d2b9f2d08a9b8f57a3f0f6de1114681256d11
GREENFONTS <godwillonyewuchii@gmail.com>
2020-09-18 15:12:53 +0100

    init

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
+
+// 'https://randomuser.me/api/'
+// https://source.unsplash.com/random from splash
 let container = document.getElementById("NumContent")
 let button = document.getElementById('submitbtn')
 let input = document.getElementById('Name')
 let Select = document.getElementById('Select')
 
-
 let UserInput = {
-    Input: input.value,
-    SelectOption: Select.value
-}
+  Input: input.value,
+  SelectOption: Select.value,
+};
 function handleChange() {
-    UserInput.Input = input.value
-    UserInput.SelectOption = Select.value
+  UserInput.Input = input.value;
+  UserInput.SelectOption = Select.value;
 }
 
-// function Loadata(){
+function Loadata(){
 
-//     if (UserInput.SelectOption == 'random') {
-//         fetch(`http://numbersapi.com/random/${UserInput.Input}?json`)
-//             .then((res) => res.json())
-//             .then((data) => {
-//                 document.getElementById("NumContent").innerHTML = data.text
-//             })
-//     }
+    if (UserInput.SelectOption == 'random') {
+        fetch(`http://numbersapi.com/random/${UserInput.Input}?json`)
+            .then((res) => res.json())
+            .then((data) => {
+                document.getElementById("NumContent").innerHTML = data.text
+            })
+    }
 
-//     else {
-//         fetch(`http://numbersapi.com/${UserInput.Input}/${UserInput.SelectOption}?json`)
-//             .then((res) => res.json())
-//             .then((data) => {
-//                 document.getElementById("NumContent").innerHTML = data.text
-//             })
-//     }
-// }
-
-function Loadata(select, value) {
-    fetch(`http://numbersapi.com/${select == 'random' ? "random/" : ""}${parseInt(value)}${select == 'random' ? '' : "/" + select}?json`)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            document.getElementById("NumContent").innerHTML = data.text
-        })
+    else {
+        fetch(`http://numbersapi.com/${UserInput.Input}/${UserInput.SelectOption}?json`)
+            .then((res) => res.json())
+            .then((data) => {
+                document.getElementById("NumContent").innerHTML = data.text
+            })
+    }
 }
-button.addEventListener('click', () => { Loadata(UserInput.SelectOption, UserInput.Input)})
+
+button.addEventListener("click", Loadata);
+
+
+
+
+
